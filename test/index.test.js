41c5155ff5ac8f4242402c57dd32f50eb56ac3b6
Kaique Jackson Souza Silva <129568316+Jackson0Souza@users.noreply.github.com>
2024-08-20 14:44:07 +0000

    teste verdadeiro feito

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,39 @@
-function somar(a, b){
-  return a + b
+function calcular(a, b, operador){
+  switch (operador) {
+    case '+':
+      return a + b
+      break;
+
+      case '-':
+      return a - b
+      break;
+
+      case '*':
+      return a * b
+      break;
+
+      case '/':
+      return a / b
+      break;
+  
+    default:
+      break;
+  }
 
 };
 
 test('quero a soma de 1 e 1', function (){
-  expect(somar(1, 1)).toBe(2);
+  expect(calcular(1, 1, '+')).toBe(2);
+});
 
+test('quero a subtração de 1 e 1', function (){
+  expect(calcular(1, 1, '-')).toBe(0);
+});
+
+test('quero a multiplicação de 1 e 1', function (){
+  expect(calcular(1, 1, '*')).toBe(1);
+});
+
+test('quero a divisão de 1 e 1', function (){
+  expect(calcular(1, 1, '/')).toBe(1);
 });
