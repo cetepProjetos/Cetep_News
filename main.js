5aeea99c14e3fe9881a72f6a9c777779ea181bcd
surdomudo <140193686+surdomudo@users.noreply.github.com>
2024-10-03 19:17:10 +0000

    Adicionei a parte de aumentar as visualizações

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
@@ -10,78 +10,101 @@ const noticias = [
     sku: "amanha-visita-secretaria",
     titulo: "Amanhã receberemos como visita a secretária.",
     data: "2024-09-28",
-    planoDeFundo: "https://www.scurra.com.br/blog/dicas-para-criar-reunioes-online-e-com-resultados-exponenciais/",
-    inconDaNoticia: "https://lspost.com.br/blog/softwares/plataforma-para-reuniao-online-facilita-comunicacao-entre-funcionarios/",
+    planoDeFundo:
+      "https://www.scurra.com.br/blog/dicas-para-criar-reunioes-online-e-com-resultados-exponenciais/",
+    inconDaNoticia:
+      "https://lspost.com.br/blog/softwares/plataforma-para-reuniao-online-facilita-comunicacao-entre-funcionarios/",
     descricao: "Vamos receber a visita",
     curtidas: 56,
-    visualizacao: 100
+    visualizacao: 100,
   },
   {
     id: "2121",
     sku: "aluna-calçola-professor",
     titulo: "Aluna joga calçola no professor em sala de aula!",
     data: "2024-09-28",
-    planoDeFundo: "https://www.scurra.com.br/blog/dicas-para-criar-reunioes-online-e-com-resultados-exponenciais/",
-    inconDaNoticia: "https://lspost.com.br/blog/softwares/plataforma-para-reuniao-online-facilita-comunicacao-entre-funcionarios/",
+    planoDeFundo:
+      "https://www.scurra.com.br/blog/dicas-para-criar-reunioes-online-e-com-resultados-exponenciais/",
+    inconDaNoticia:
+      "https://lspost.com.br/blog/softwares/plataforma-para-reuniao-online-facilita-comunicacao-entre-funcionarios/",
     descricao: "Vamos receber a visita",
     curtidas: 77,
-    visualizacao: 148
+    visualizacao: 148,
   },
   {
     id: "2121",
     sku: "pe-de-meia-corta-cetep",
     titulo: "Governo corta o programa Pé-De-Meia no CETEP!",
     data: "2024-09-28",
-    planoDeFundo: "https://www.scurra.com.br/blog/dicas-para-criar-reunioes-online-e-com-resultados-exponenciais/",
-    inconDaNoticia: "https://lspost.com.br/blog/softwares/plataforma-para-reuniao-online-facilita-comunicacao-entre-funcionarios/",
+    planoDeFundo:
+      "https://www.scurra.com.br/blog/dicas-para-criar-reunioes-online-e-com-resultados-exponenciais/",
+    inconDaNoticia:
+      "https://lspost.com.br/blog/softwares/plataforma-para-reuniao-online-facilita-comunicacao-entre-funcionarios/",
     descricao: "Vamos receber a visita",
     curtidas: 75,
-    visualizacao: 107
+    visualizacao: 107,
   },
   {
     id: "2121",
     sku: "inauguração-sala-jogos-cetep",
     titulo: "Nesta segunda-feira será a inauguração da sala de jogos no CETEP!",
     data: "2024-09-28",
-    planoDeFundo: "https://www.scurra.com.br/blog/dicas-para-criar-reunioes-online-e-com-resultados-exponenciais/",
-    inconDaNoticia: "https://lspost.com.br/blog/softwares/plataforma-para-reuniao-online-facilita-comunicacao-entre-funcionarios/",
+    planoDeFundo:
+      "https://www.scurra.com.br/blog/dicas-para-criar-reunioes-online-e-com-resultados-exponenciais/",
+    inconDaNoticia:
+      "https://lspost.com.br/blog/softwares/plataforma-para-reuniao-online-facilita-comunicacao-entre-funcionarios/",
     descricao: "Vamos receber a visita",
     curtidas: 288,
-    visualizacao: 400
+    visualizacao: 400,
   },
   {
     id: "2121",
     sku: "falta-agua-cetep",
     titulo: "Esta semana não haverá aula devido a falta de água no CETEP.",
     data: "2024-09-28",
-    planoDeFundo: "https://www.scurra.com.br/blog/dicas-para-criar-reunioes-online-e-com-resultados-exponenciais/",
-    inconDaNoticia: "https://lspost.com.br/blog/softwares/plataforma-para-reuniao-online-facilita-comunicacao-entre-funcionarios/",
+    planoDeFundo:
+      "https://www.scurra.com.br/blog/dicas-para-criar-reunioes-online-e-com-resultados-exponenciais/",
+    inconDaNoticia:
+      "https://lspost.com.br/blog/softwares/plataforma-para-reuniao-online-facilita-comunicacao-entre-funcionarios/",
     descricao: "Vamos receber a visita",
     curtidas: 55,
-    visualizacao: 78
+    visualizacao: 78,
   },
 ];
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
-app.get("/noticias", function(req, res) {
+app.get("/noticias", function (req, res) {
   res.json(noticias);
 });
 
-
-app.get("/noticias/tops", function(req, res) {
+app.get("/noticias/tops", function (req, res) {
   res.json(noticias);
 });
 
-app.get("/noticias/:slog", function(req, res) {
-  res.json(req.params.slog);
+app.get("/noticias/:slug/views", function (req, res) {
+  const slug = req.params.slug;
+
+  console.log(slug);
+
+  for (var i = 0; i < noticias.length; i++) {
+    console.log("i", i);
+    if (noticias[i].sku == slug) {
+      console.log("encontrou");
+      noticias[i].visualizacao++;
+      res.sendStatus(204);
+      return;
+    }
+  }
+  res.sendStatus(404);
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Servidor subiu na porta " + port);
 });
+
 // app.get("/noticias/:sku", function(req, res) {
 //   const sku = req.params.sku;
 
