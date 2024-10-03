adbe005053f5164d6c56f1fda37c0dbee9fe4616
Kaique Jackson Souza Silva <129568316+Jackson0Souza@users.noreply.github.com>
2024-10-03 18:55:57 +0000

    APIs

--- main.js
+++ main.js
@@ -15,7 +15,51 @@ const noticias = [
     descricao: "Vamos receber a visita",
     curtidas: 56,
     visualizacao: 100
-  }
+  },
+  {
+    id: "2121",
+    sku: "aluna-calçola-professor",
+    titulo: "Aluna joga calçola no professor em sala de aula!",
+    data: "2024-09-28",
+    planoDeFundo: "https://www.scurra.com.br/blog/dicas-para-criar-reunioes-online-e-com-resultados-exponenciais/",
+    inconDaNoticia: "https://lspost.com.br/blog/softwares/plataforma-para-reuniao-online-facilita-comunicacao-entre-funcionarios/",
+    descricao: "Vamos receber a visita",
+    curtidas: 77,
+    visualizacao: 148
+  },
+  {
+    id: "2121",
+    sku: "pe-de-meia-corta-cetep",
+    titulo: "Governo corta o programa Pé-De-Meia no CETEP!",
+    data: "2024-09-28",
+    planoDeFundo: "https://www.scurra.com.br/blog/dicas-para-criar-reunioes-online-e-com-resultados-exponenciais/",
+    inconDaNoticia: "https://lspost.com.br/blog/softwares/plataforma-para-reuniao-online-facilita-comunicacao-entre-funcionarios/",
+    descricao: "Vamos receber a visita",
+    curtidas: 75,
+    visualizacao: 107
+  },
+  {
+    id: "2121",
+    sku: "inauguração-sala-jogos-cetep",
+    titulo: "Nesta segunda-feira será a inauguração da sala de jogos no CETEP!",
+    data: "2024-09-28",
+    planoDeFundo: "https://www.scurra.com.br/blog/dicas-para-criar-reunioes-online-e-com-resultados-exponenciais/",
+    inconDaNoticia: "https://lspost.com.br/blog/softwares/plataforma-para-reuniao-online-facilita-comunicacao-entre-funcionarios/",
+    descricao: "Vamos receber a visita",
+    curtidas: 288,
+    visualizacao: 400
+  },
+  {
+    id: "2121",
+    sku: "falta-agua-cetep",
+    titulo: "Esta semana não haverá aula devido a falta de água no CETEP.",
+    data: "2024-09-28",
+    planoDeFundo: "https://www.scurra.com.br/blog/dicas-para-criar-reunioes-online-e-com-resultados-exponenciais/",
+    inconDaNoticia: "https://lspost.com.br/blog/softwares/plataforma-para-reuniao-online-facilita-comunicacao-entre-funcionarios/",
+    descricao: "Vamos receber a visita",
+    curtidas: 55,
+    visualizacao: 78
+  },
 ];
 
 app.get("/", function(req, res) {
@@ -26,32 +70,41 @@ app.get("/noticias", function(req, res) {
   res.json(noticias);
 });
 
+
+app.get("/noticias/tops", function(req, res) {
+  res.json(noticias);
+});
+
+app.get("/noticias/:slog", function(req, res) {
+  res.json(req.params.slog);
+});
+
 app.listen(port, function() {
   console.log("Servidor subiu na porta " + port);
 });
-app.get("/noticias/:sku", function(req, res) {
-  const sku = req.params.sku;
+// app.get("/noticias/:sku", function(req, res) {
+//   const sku = req.params.sku;
 
-  for(const noticia of noticias) {
-    if (noticia.sku == sku) {
-      res.json(noticia);
-      return;
-    }
-  }
+//   for(const noticia of noticias) {
+//     if (noticia.sku == sku) {
+//       res.json(noticia);
+//       return;
+//     }
+//   }
 
-  res.sendStatus(404);
-});
+//   res.sendStatus(404);
+// });
 
-app.post("/noticias", function(req, res) {
-  const corpoDaNoticia = req.body;
-  noticias.push(corpoDaNoticia);
-  res.sendStatus(200);
-});
+// app.post("/noticias", function(req, res) {
+//   const corpoDaNoticia = req.body;
+//   noticias.push(corpoDaNoticia);
+//   res.sendStatus(200);
+// });
 
-app.listen(port, function() {
-  console.log("Subiu o servidor");
-});
+// app.listen(port, function() {
+//   console.log("Subiu o servidor");
+// });
 
-app.get("/noticias/tops", function(req, res) {
-  res.send("Tops notícias");
-});
+// app.get("/noticias/tops", function(req, res) {
+//   res.send("Tops notícias");
+// });
