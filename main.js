6d3ac0eff1b074338b48dc513a8c04a9e12e1525
Kaique Jackson Souza Silva <129568316+Jackson0Souza@users.noreply.github.com>
2024-10-18 19:59:41 +0000

    HTML dinamico - Home

--- main.js
+++ main.js
@@ -10,8 +10,44 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-  res.render("blog/home", { titulo: "TEST" });
+  res.render("blog/home", { 
+    metaDescricao: "Bem vindo ao CETEP News! Você sai daqui atusalizado de tudo." ,
+    metaKeywords: "cetep news, blog, macubas, noticias, semana, principais, tops, ",
+    metaTitle: "CETEP NEWS",
+    metaUrlImage: "URL",
+    metaUrl: "URL",
+    noticiasRecentes: [  {
+      imageDeFundo: 'https://jornalfato.com.br/files/f3df38bea0571d15e376bda9c1245e59/midia_foto/20210704/aviaocai.jpg',
+      descricao: 'Avião caiu no interior da Bahia',
+      visualizacoes: 377,
+      curtidas: 87
+      },],
+    topsNoticiasSemanais: [
+      {
+      imageDeFundo: 'https://jornalfato.com.br/files/f3df38bea0571d15e376bda9c1245e59/midia_foto/20210704/aviaocai.jpg',
+      descricao: 'Avião caiu no interior da Bahia',
+      visualizacoes: 377,
+      curtidas: 87
+      },
+      {
+        imageDeFundo: 'https://tse1.explicit.bing.net/th?id=OIP.ETspXUj4R0T_mY18xGsLYgHaEy&pid=Api&P=0&h=180',
+        descricao: 'E agora?!!!!',
+        visualizacoes: 2000,
+        curtidas: 455
+        },
+        {
+          imageDeFundo: 'https://midia.agoranordeste.com.br/uploads/imagens/pelo_brasil/amazonia_queimadas.png',
+          descricao: 'A Amazonas chora.',
+          visualizacoes: 554,
+          curtidas: 67
+          },
+    ]
+  
+
+  });
 });
+
+
 
 app.get("/noticias", function (req, res) {
   res.json([]);
